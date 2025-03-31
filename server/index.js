f3377f6b1eb88299135cc5e17df56cde9d1c554a
HarshAgarwal4 <agarwalh270@gmail.com>
2025-03-31 16:21:43 +0530

    final1

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+let cors = require('cors')
 
 const { homeroutes } = require('./app/routes/users/home');
 const { signUproute } = require('./app/routes/users/signup');
@@ -20,6 +21,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors())
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "app/views")); 
 app.use(express.static(path.join(__dirname, '../public')));
